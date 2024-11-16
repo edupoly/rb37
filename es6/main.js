a93f01b1fb00fc94b7da4c99fa25c33b23b03801
edupoly <info.edupoly@gmail.com>
2024-11-16 16:48:32 +0530

    rb37code

---
+++ es6/main.js
@@ -0,0 +1,3 @@
+import mydef,{add,sub} from './utils.js';
+console.log(add(10,30))
+console.log(mydef())
---
+++ es6/utils.js
@@ -0,0 +1,16 @@
+export const add = function(a,b){
+    return a+b;
+}
+export const sub = function(a,b){
+    return a-b;
+}
+export const mul = function(a,b){
+    return a*b;
+}
+export const div = function(a,b){
+    return a/b;
+}
+
+export default function xyz(){
+    return "Hello"
+}
